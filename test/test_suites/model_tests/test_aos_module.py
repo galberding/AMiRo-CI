36baf09acd9a6bb026d0c7ab566d64cb4b9b4503
galberding <galberding@techfak.uni-bielefeld.de>
2021-07-21 14:50:48 +0200

    Refactor tests: divide in aos module tests and flag/arg tests

--- test/test_suites/model_tests/test_aos_module.py
+++ test/test_suites/model_tests/test_aos_module.py
@@ -26,27 +26,6 @@ class TestAosModel(unittest.TestCase):
             ('USE_FPU_OPT', '-mfloat-abi=$(USE_FPU) -mfpu=fpv4-sp-d16') # Needs to be substituted
         ]
 
-
-    def test_create_arguments(self):
-        arg = Argument("-WALL")
-        self.assertEqual(arg.name, "-WALL")
-
-    def test_create_flag(self):
-        name = "flag"
-        wall = "-WALL"
-        dtest = "-Dtest"
-        arg_string = " ".join([wall, dtest])
-        args = [Argument(wall), Argument(dtest)]
-        flag = Flag(name, arg_string)
-        self.assertEqual(flag.name, name)
-        self.assertEqual(flag.argument_str, arg_string)
-        self.assertEqual(set(flag.args),set(args))
-
-    def test_flag_has_unresolved_arguments(self):
-        # example: -mfloat-abi=$(USE_FPU)
-        unresolved_flag = Flag('USE_FPU_OPT', '-mfloat-abi=$(USE_FPU) -mfpu=fpv4-sp-d16')
-        self.assertFalse(unresolved_flag.is_resolved())
-
     def test_create_module(self):
         self.assertEqual(self.aos_module.name, self.module_name)
 
@@ -85,21 +64,6 @@ class TestAosModel(unittest.TestCase):
         self.assertEqual(len(sub_flag_names), 1)
         self.assertEqual(sub_flag_names[0], 'USE_FPU')
 
-    def test_no_substitution_flag_in_resolved_argument(self):
-        resolved_flag = Flag(
-            'USE_FPU_OPT',
-            '-mfloat-abi=no -mfpu=fpv4-sp-d16')
-        self.assertTrue(resolved_flag.is_resolved())
-        sub_flag_names = resolved_flag.get_substitution_flag_names()
-        self.assertEqual(sub_flag_names, list())
-
-    def test_get_substitution_flag_from_argument_multiple_sub_flag(self):
-        unresolved_flag = Flag(
-            'USE_FPU_OPT',
-            '-mfloat-abi=$(USE_FPU) -mfpu=$(USE_MPU_TYPE)')
-        sub_flag_names = unresolved_flag.get_substitution_flag_names()
-        self.assertEqual(sub_flag_names, ['USE_FPU', 'USE_MPU_TYPE'])
-
     def test_module_substitute_unresolved_flag(self):
         self.aos_module.create_flags(self.search_results)
         u_flags = self.aos_module.get_unresolved_flags()
@@ -119,19 +83,6 @@ class TestAosModel(unittest.TestCase):
             FlagNotFoundException,
             self.aos_module.find_flag_by_name,
             'USE_FPU')
-
-    def test_flag_resolve_unresolved_flag(self):
-        unresolved_flag = Flag(
-            'USE_FPU_OPT',
-            '-mfloat-abi=$(USE_FPU)')
-        self.assertFalse(unresolved_flag.is_resolved())
-        self.assertTrue(unresolved_flag.resolve(
-            Flag("USE_FPU", "fpu_value")))
-        self.assertTrue(unresolved_flag.is_resolved())
-        self.assertEqual(
-            unresolved_flag.args[0].name,
-            '-mfloat-abi=fpu_value'
-        )
 
     def test_module_resolve_module(self):
         self.aos_module.create_flags(self.search_results)
---
+++ test/test_suites/model_tests/test_flag_and_argument.py
@@ -0,0 +1,58 @@
+from ..test_utils.path_helper import PathHelper
+import unittest
+
+from amirotest.model.aos_model import Argument, Flag, FlagNotFoundException, Module
+from amirotest.tools.makefile_search import MakefileSearch
+
+
+class TestArgumentModel(unittest.TestCase):
+    def test_create_arguments(self):
+        arg = Argument("-WALL")
+        self.assertEqual(arg.name, "-WALL")
+
+    def test_create_flag(self):
+        name = "flag"
+        wall = "-WALL"
+        dtest = "-Dtest"
+        arg_string = " ".join([wall, dtest])
+        args = [Argument(wall), Argument(dtest)]
+        flag = Flag(name, arg_string)
+        self.assertEqual(flag.name, name)
+        self.assertEqual(flag.argument_str, arg_string)
+        self.assertEqual(set(flag.args),set(args))
+
+    def test_flag_has_unresolved_arguments(self):
+        # example: -mfloat-abi=$(USE_FPU)
+        unresolved_flag = Flag(
+            'USE_FPU_OPT',
+            '-mfloat-abi=$(USE_FPU) -mfpu=fpv4-sp-d16')
+        self.assertFalse(unresolved_flag.is_resolved())
+
+
+    def test_no_substitution_flag_in_resolved_argument(self):
+        resolved_flag = Flag(
+            'USE_FPU_OPT',
+            '-mfloat-abi=no -mfpu=fpv4-sp-d16')
+        self.assertTrue(resolved_flag.is_resolved())
+        sub_flag_names = resolved_flag.get_substitution_flag_names()
+        self.assertEqual(sub_flag_names, list())
+
+    def test_get_substitution_flag_from_argument_multiple_sub_flag(self):
+        unresolved_flag = Flag(
+            'USE_FPU_OPT',
+            '-mfloat-abi=$(USE_FPU) -mfpu=$(USE_MPU_TYPE)')
+        sub_flag_names = unresolved_flag.get_substitution_flag_names()
+        self.assertEqual(sub_flag_names, ['USE_FPU', 'USE_MPU_TYPE'])
+
+    def test_flag_resolve_unresolved_flag(self):
+        unresolved_flag = Flag(
+            'USE_FPU_OPT',
+            '-mfloat-abi=$(USE_FPU)')
+        self.assertFalse(unresolved_flag.is_resolved())
+        self.assertTrue(unresolved_flag.resolve(
+            Flag("USE_FPU", "fpu_value")))
+        self.assertTrue(unresolved_flag.is_resolved())
+        self.assertEqual(
+            unresolved_flag.args[0].name,
+            '-mfloat-abi=fpu_value'
+        )
