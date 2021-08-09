5e672d82232bc8cf4aa863abef8d171716ffca38
galberding <galberding@techfak.uni-bielefeld.de>
2021-08-09 10:27:26 +0200

    Add test to ensure that the config matrix is correctly created

--- src/amirotest/tools/config/conf_matrix_builder.py
+++ src/amirotest/tools/config/conf_matrix_builder.py
@@ -3,36 +3,52 @@ import pandas as pd
 
 class ConfMatrixBuilder:
     def build_dataframe_config(self, config: dict[str, list[Any]]) -> pd.DataFrame:
+        """Creates matrix where all parameters passed by the config are combined with each other.
+        config: dict in form of: {"ParamerterName": ["list", "of", "Values"], ...}
+        return  configuration matrix in form of a DataFrame where the columns hold the parameter names.
+        """
         col_names = config.keys()
 
         aug_conf = self.augment_config(config)
         # return pd.DataFrame(aug_conf)
-        return pd.DataFrame(aug_conf, columns=col_names)
+        return pd.DataFrame(aug_conf, columns=col_names, dtype="string")
 
     def augment_config(self, config: dict[str, list[Any]]) -> list[list]:
+        """Combine all config values with each other
+        """
         row, _ = self._get_conf_mat_size(config)
         tmp_conf = {}
-        mat: list[tuple] = []
+        mat: list[list] = []
         for opt, args in config.items():
             # print(args)
-            mat = self._append_col(args, mat)
+
+            mat = self._insert_in_config_mat(args, mat)
         return mat
 
-    def _append_col(self, col: list[Any], mat: list[tuple[Any]]):
-        new_mat: list[list] = []
+    def _insert_in_config_mat(self, col: list[Any], mat: list[list[Any]]):
+        """Combine all values in mat with all values in col.
+        """
         if not mat:
-            for i in col:
-                print(i)
-                new_mat.append([i])
-            # print("New mat:", new_mat)
-            return new_mat
+            return self._populate_matrix(col)
+
+        new_mat: list[list] = []
         for i in col:
             for row in mat:
-                # print(type(row))
-                new_mat.append([i, *row])
+                new_mat.append([*row, i])
+        return new_mat
+
+    def _populate_matrix(self, col: list[Any]):
+        """Create matrix construct and fill with col values.
+        """
+        new_mat = []
+        for i in col:
+                # print(i)
+                new_mat.append([i])
         return new_mat
 
     def _get_conf_mat_size(self, config: dict[str, list[Any]]) -> tuple[int, int]:
+        """Return row and column count.
+        """
         col_count = len(config.items())
         row_count = 1
         for _, args in config.items():
--- test/test_suites/tool_tests/test_conf_matrix_builder.py
+++ test/test_suites/tool_tests/test_conf_matrix_builder.py
@@ -5,25 +5,35 @@ from amirotest.tools.config import ConfMatrixBuilder
 
 class TestConfMatrixBuilder(unittest.TestCase):
     def setUp(self) -> None:
-        self.conf = {"opt1": [1,2], "opt2": ["A", "B"], "opt3": ["true", "false", "None"]}
+        self.conf = {"opt1": ["1","2"], "opt2": ["A", "B"], "opt3": ["true", "false", "None"], "opt4": ["42", "44", "45"]}
         self.c_build = ConfMatrixBuilder()
 
     def test_get_row_col_counts(self):
+        conf = {"opt1": [1,2], "opt2": ["A", "B"], "opt3": ["true", "false", "None"]}
         cols = 3
         rows = 12
-        rows_cols = self.c_build._get_conf_mat_size(self.conf)
+        rows_cols = self.c_build._get_conf_mat_size(conf)
         self.assertEqual(rows_cols[0], rows)
         self.assertEqual(rows_cols[1], cols)
     # create empty matrix
     def test_augment_config(self):
-        rows, cols = self.c_build._get_conf_mat_size(self.conf)
+        _, cols = self.c_build._get_conf_mat_size(self.conf)
         aug_conf = self.c_build.augment_config(self.conf)
-        print(aug_conf)
+        # print(aug_conf)
         for row in aug_conf:
             self.assertEqual(cols, len(row))
 
-    def test_build_dataframe(self):
+    def test_ensure_correct_parameter_names_in_combined_config(self):
+        """Checks two conditions:
+        1. The Parameter names are assigned to the right column
+        2. The columns are created correctly
+        """
         df: pd.DataFrame = self.c_build.build_dataframe_config(self.conf)
-        print(df)
-        shape = df.shape
-        self.assertEqual((12,3), shape)
+        rows, _ = self.c_build._get_conf_mat_size(self.conf)
+        for parameter_name, parameters in self.conf.items():
+            val_count = df[parameter_name].value_counts()
+            item_count = val_count.shape[0]
+            for param in parameters:
+                # print("REs:",)
+                res = df[parameter_name].str.contains(param)
+                self.assertEqual(res[res == True].shape[0], rows // item_count)
