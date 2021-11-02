4d358bd2ee5347a6734acc04b8140fc792ea2cf9
galberding <galberding@techfak.uni-bielefeld.de>
2021-11-02 19:16:36 +0100

    Fix packages

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 import setuptools
 
+
+test_deps = [
+    'pytest',
+    'flake8',
+    'pylint',
+    'mypy',
+]
+
+extras = {
+    'test': test_deps
+}
+
+
 setuptools.setup(
-    name="confTester",
-    version="0.1",
-    author="Georg Alberding",
-    author_email="galberding@uni-bielefeld.de",
+    name='AMiRo-CI',
+    version='0.1',
+    author='Georg Alberding',
+    author_email='',
     description=
-    "Package utilizing conditional compilation to test the AMiRo-OS and AMiRo-Apps project.",
+    'Package utilizing conditional compilation to test the AMiRo-OS and AMiRo-Apps project.',
     install_requires=['PyYAML', 'tqdm', 'overrides', 'pandas'],
     classifiers=[
-        "Programming Language :: Python :: 3.9",
-        # "License :: OSI Approved :: MIT License",
-        # "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)'
     ],
     scripts=['src/amiroci/amiroCI'],
-    packages=setuptools.find_packages(
-        exclude=("test*", "sim*", "doc*", "examples*")
-    ),
-    # python_requires=">=3.7",
+    python_requires=">=3.9",
     package_dir={"": "src"},
-    include_package_data=True,
-    extras_require=dict(test=['testfixtures'], ),
-    test_suite="tests"
+    packages=[
+        'amiroci',
+        'amiroci.tools',
+        'amiroci.tools.config',
+        'amiroci.tools.search',
+        'amiroci.tools.search.search_result',
+        'amiroci.model',
+        'amiroci.model.argument',
+        'amiroci.model.option',
+        'amiroci.controller',
+    ],
+    tests_require=test_deps,
+    extras_require=extras,
 )
